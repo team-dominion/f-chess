f0da9bbc596e97fe896f16f841e55f8f25da9a4f
haseaki0503 <contact@haseaki.com>
2015-05-05 15:18:26 +0900

    clean up

--- draw.js
+++ draw.js
@@ -1,28 +1,33 @@
-const squares_number = 17;
-const playground_width = 600;
+var PLAYGROUND_WIDTH = 600;
+var NUMBER_OF_SQUARE = 17;
+var NUMBER_OF_PLAYGROUND_LINE = NUMBER_OF_SQUARE / PLAYGROUND_WIDTH;
 
 $(function(){
   /* canvas */
   playGround = $("#play-ground").get(0);
   ctxCanvas  = playGround.getContext("2d");
+
+  /* Load function */
   drawField();
 });
 
 function drawField(){
   var i;
-  for (i=0; i<=17;i++){
-    if((i === 0) || (i === squares_number)){
+
+  for (i=0; i<=17;i++) {
+    if (i === 0 || i === NUMBER_OF_SQUARE) {
       ctxCanvas.beginPath();
       ctxCanvas.lineWidth = 10.0;
     } else {
       ctxCanvas.beginPath();
       ctxCanvas.lineWidth = 1.0;
     }
-    ctxCanvas.moveTo(i * playground_width/(squares_number),0);
-    ctxCanvas.lineTo(i * playground_width/(squares_number), playground_width);
+    ctxCanvas.moveTo(i * PLAYGROUND_WIDTH/NUMBER_OF_SQUARE,0);
+    ctxCanvas.lineTo(i * PLAYGROUND_WIDTH/NUMBER_OF_SQUARE, PLAYGROUND_WIDTH);
     ctxCanvas.stroke();
-    ctxCanvas.moveTo(0,i*playground_width/(squares_number));
-    ctxCanvas.lineTo(playground_width, i*playground_width/(squares_number));
+    ctxCanvas.moveTo(0, i * PLAYGROUND_WIDTH/NUMBER_OF_SQUARE);
+    ctxCanvas.lineTo(PLAYGROUND_WIDTH, i * PLAYGROUND_WIDTH/NUMBER_OF_SQUARE);
     ctxCanvas.stroke();
   }
+
 }
