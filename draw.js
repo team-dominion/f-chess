3ff40a1111d88da753d95899a98ae991d72dc8c3
daisyo <ngdsyma1@gmail.com>
2015-05-05 15:37:34 +0900

    clean up

--- draw.js
+++ draw.js
@@ -1,6 +1,6 @@
 var PLAYGROUND_WIDTH = 600;
 var NUMBER_OF_SQUARE = 17;
-var NUMBER_OF_PLAYGROUND_LINE = NUMBER_OF_SQUARE / PLAYGROUND_WIDTH;
+var NUMBER_OF_PLAYGROUND_LINE = PLAYGROUND_WIDTH / NUMBER_OF_SQUARE;
 
 $(function(){
   /* canvas */
@@ -14,7 +14,7 @@ $(function(){
 function drawField(){
   var i;
 
-  for (i=0; i<=17;i++) {
+  for (i=0; i<=17; i++) {
     if (i === 0 || i === NUMBER_OF_SQUARE) {
       ctxCanvas.beginPath();
       ctxCanvas.lineWidth = 10.0;
@@ -22,11 +22,11 @@ function drawField(){
       ctxCanvas.beginPath();
       ctxCanvas.lineWidth = 1.0;
     }
-    ctxCanvas.moveTo(i * PLAYGROUND_WIDTH/NUMBER_OF_SQUARE,0);
-    ctxCanvas.lineTo(i * PLAYGROUND_WIDTH/NUMBER_OF_SQUARE, PLAYGROUND_WIDTH);
+    ctxCanvas.moveTo(i * NUMBER_OF_PLAYGROUND_LINE,0);
+    ctxCanvas.lineTo(i * NUMBER_OF_PLAYGROUND_LINE, PLAYGROUND_WIDTH);
     ctxCanvas.stroke();
-    ctxCanvas.moveTo(0, i * PLAYGROUND_WIDTH/NUMBER_OF_SQUARE);
-    ctxCanvas.lineTo(PLAYGROUND_WIDTH, i * PLAYGROUND_WIDTH/NUMBER_OF_SQUARE);
+    ctxCanvas.moveTo(0, i * NUMBER_OF_PLAYGROUND_LINE);
+    ctxCanvas.lineTo(PLAYGROUND_WIDTH, i * NUMBER_OF_PLAYGROUND_LINE);
     ctxCanvas.stroke();
   }
 
